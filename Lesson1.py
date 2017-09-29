b0b84ac8d7caf5321d4924b391684ba767684151
andrewquartic <andrew@quarticsolutions.com>
2017-09-28 23:14:34 -0700

    adding additional farm logic

--- Lesson1.py
+++ Lesson1.py
@@ -1,4 +1,5 @@
  
+from datetime import datetime
 
 class Chicken():   
 
@@ -6,35 +7,130 @@ class Chicken():
         print("I'm a new chicken named " + name + "!")
         self.hatched = hatch
         self.myname = name
-        
+        self.last_fed = 
     def Hatch(self): 
         if self.hatched == False:
             self.hatched = True
+            print(self.myname + ": I've been hatched!")
+    
+    def GetStatus(self):
+        if (self.hatched == True):
+            return "peep peep!"
+        else:
+            return "I'm an egg!"
+
+class Farm():
+    def __init__(self):
+        self.coop = []
+
+    def addChicken(self, chick):
+        if(type(chick) is Chicken):
+            self.coop.append(chick)
+            print("")
+            print("cluck cluck!")
+        else:
+            print("that's not a chicken!")
+
+    def createChicken(self):
+        inputName = raw_input("Name a chicken: ")
+        inputHatched = raw_input("Is it hatched? ")
+
+        if inputHatched.lower() == "yes":
+            hatch = True
+            self.coop.append(Chicken(inputName, hatch))
+
+        elif inputHatched.lower() == "no":
+            hatch = False
+            self.coop.append(Chicken(inputName, hatch))
+        else:
+            print("Not a valid input! ")
+            self.createChicken()
+
+    def listChickens(self):
+        print("")
+        chickencount = len(self.coop)
+        print("we have: " + str(chickencount) + " chickens")    
+        for chicken in self.coop:
+            print(chicken.myname + ": " + chicken.GetStatus())
+
+    def checkIfHatched(self):
+        for chicken in self.coop:
+            if chicken.hatched == False:
+                print("Don't count your chickens before they've hatched!")
+                return
+
+    def getChicken(self, name):
+        for chicken in self.coop:
+            if chicken.myname == name:
+                return chicken
+
+    def hatchChicken(self, name):
+        chicken = getChicken(input_name, self.coop)
+        if(chicken == None):
+            print("")
+            print("I don't know a chicken by that name! ..screamed the stable boy")
+            return False
+        else:
+            chicken.Hatch()
+            return True
+
+def PrintOptions():
+    print("--- Options ---")
+    print("")
+    print("1: Create Chicken")
+    print("2: List Chickens")
+    print("3: Hatch Chicken")
+    print("4: Feed Chicken")
+    print("5: Quit")
+
+def GetOptionFromUser():
+    print("")
+    try:
+        return int(raw_input("Select Option: "))
+    except ValueError:
+        print("Invalid input. Please try again!")
+        GetOptionFromUser()
+
 
 # ----- Application --------
 
+# ----- function end 
+
+farm = Farm()
+
 chicken1 = Chicken("Peaches", True)
 chicken2 = Chicken("Charlie", True)
-chicken3 = Chicken("Princess", False)
-chicken4 = Chicken("Constatine", False)
+chicken3 = Chicken("Princess", True)
+chicken4 = Chicken("Constantine", True)
 
-coop = []
-coop.append(chicken1)
-coop.append(chicken2)
-coop.append(chicken3)
-coop.append(chicken4)
-
-print("")
-
-chickencount = len(coop)
-print("we have: " + str(chickencount) + " chickens")
-
-print("")
-
-for chicken in coop:
-    print(chicken.myname)
-    if chicken.hatched == False:
-        print("Don't count your chickens before they've hatched!")
+farm.addChicken(chicken1)
+farm.addChicken(chicken2)
+farm.addChicken(chicken3)
+farm.addChicken(chicken4)
 
 
+avariable = True
+while(avariable == True):
+    print("")
+    PrintOptions()
 
+    input_option = GetOptionFromUser()
+    print("")
+
+    if input_option == 1:
+        farm.createChicken()
+        
+    elif input_option == 2:
+        farm.listChickens()
+
+    elif input_option == 3:
+        input_name = raw_input("Name a chicken to hatch: ")
+        farm.hatchChicken(input_name)
+
+    print("")
+    input_from_user = raw_input("Quit? (Y / N): ")
+
+    if input_from_user.upper() == "Y":
+        avariable = False
+    elif input_from_user.upper() == "N":
+        avariable = True
